182f827220d666d317082debf5df821c08c944a2
Abdelrahman <31183876+abdelrahmann22@users.noreply.github.com>
2024-04-07 01:56:54 +0200

    check

---
+++ broswer_automation_and_web_scrapping/scrape_dynamic_value_with_selenium.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+def get_driver():
+    options = webdriver.EdgeOptions()
+    options.add_argument("disable-infobars")
+    options.add_argument("start-maximized")
+    options.add_argument("disable-dev-shm-usage")
+    options.add_argument("no-sandbox")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_argument("disable-blink-features=AutomationControlled")
+
+    driver = webdriver.Edge(options=options)
+    driver.get("https://automated.pythonanywhere.com/")
+    return driver
+
+def main():
+    driver = get_driver()
+    element = driver.find_element(By.XPATH, "/div")
+    return element.text
+
+print(main())
